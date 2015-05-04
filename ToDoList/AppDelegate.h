7bba4dc793bb8981d3c7d2708f02fe8fd96dab14
Bret Comnes <bcomnes@gmail.com>
2015-05-04 10:56:55 -0700

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Bret Comnes on 5/4/15.
+//  Copyright (c) 2015 Bret Comnes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
